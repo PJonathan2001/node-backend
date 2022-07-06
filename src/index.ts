ff81e80a2aff7eee66d4e1bdcc486494f57b785d
PJonathan2001 <87325282+PJonathan2001@users.noreply.github.com>
2022-07-05 21:27:57 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { PORT } from './config';
 import { connectToMongodb } from "./database";
 import { router } from './router';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const main = async () => {
     await connectToMongodb();
     const server = express();
     server.use(express.json());
+    server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));  
     router(server);
 
     //Start server
